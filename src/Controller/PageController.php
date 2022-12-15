04365c339cd564cc5fa922f7b804e93ed8569512
kotou-boina <kotou.boina@gmail.com>
2022-12-15 19:33:09 +0100

    add dashboard page

---
+++ src/Controller/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route(path: '/', name: 'dashboard')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function dashboard()
+    {
+        return $this->render('pages/dashboard.html.twig');
+    }
+}
