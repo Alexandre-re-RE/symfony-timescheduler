0b1725ba80e9f9d6f27bc8e3e3af239f95752c1a
kotou-boina <kotou.boina@gmail.com>
2023-01-06 10:54:07 +0100

    refactor app.js

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,12 @@ import "./timesh/vendor/css/theme-default.css"
 import "./timesh/css/demo.css"
 import "./timesh/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"
 import "./timesh/vendor/libs/apex-charts/apex-charts.css"
+import "./timesh/vendor/css/pages/page-auth.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'fontawesome-free/css/all.min.css'
+import '../public/css/jkanban.min.css'
+import 'toastr/build/toastr.css'
+
 import "./timesh/vendor/js/helpers.js"
 import "./timesh/js/config.js"
 import "./timesh/vendor/libs/jquery/jquery.js"
@@ -20,13 +26,6 @@ import "./timesh/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"
 import "./timesh/vendor/js/menu.js"
 import "./timesh/js/main.js"
 import "./timesh/js/ui-popover.js"
-import "./timesh/vendor/css/pages/page-auth.css"
-
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'fontawesome-free/css/all.min.css'
-import '../public/css/jkanban.min.css'
-import 'toastr/build/toastr.css'
 
 import toastr from 'toastr'
 import { Calendar } from '@fullcalendar/core'
